162ab28285cda84379c39fc202af2549e538a404
sunshine-lin <17328850174@163.com>
2017-08-16 19:09:22 +0800

    新增vue文件夹

---
+++ vue/filters.js
@@ -0,0 +1,11 @@
+/**
+ * vue公共组件js
+ * @authors Your Name (you@example.org)
+ * @date    2017-08-08 11:03:08
+ * @version $Id$
+ */
+import Vue from 'vue'
+
+Vue.filter("sum", function(value) {
+    return value + 4;
+});
