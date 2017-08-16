b3eab66e31a77d1e2c1d213ebeed3089b7603d9b
sunshine-lin <you@example.com>
2017-08-16 21:57:13 +0800

    增加了自定义指令

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <input type="number" v-model.number="value" v-focus="red">
+    <div>{{value | toFixed | fillZero}}</div>
+    <div>{{0.1 | toFixed}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      value: 1,
+      num: 10.232323
+    }
+  },
+  // computed: {
+  //   numFixed () {
+  //     return  this.value.toFixed(2)
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+import '@/assets/vue/filters.js'
+import '@/assets/vue/directives.js'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
